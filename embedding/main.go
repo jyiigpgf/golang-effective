6b9197513a6ae3ea825eb583bb8cdef81e16d1cc
joker <jyiigpgf@gmail.com>
2020-03-31 18:31:17 +0800

    add

--- embedding/main.go
+++ embedding/main.go
@@ -1,6 +1,26 @@
 package main
 
+import "fmt"
+
+type git struct {
+	Name string
+}
+
+func (g *git)print()  {
+	fmt.Println(g.Name)
+}
+
+type github struct {
+	*git
+}
+
+func (g *github)print()  {
+	fmt.Println(g.Name)
+}
+
 func main() {
 
-	
+	_git := &git{Name:"git"}
+	g := &github{git:_git}
+	g.print()
 }
